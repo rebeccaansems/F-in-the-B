d7ae3f48ef60fc71d6eea5ce1cc54f088f653531
Rebecca Ansems <ansems_rebecca@hotmail.com>
2017-12-03 15:09:32 -0400

    lol forgot to not try to have ios build menu stuff

--- Assets/Editor Code/MenuItems.cs
+++ Assets/Editor Code/MenuItems.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿#if UNITY_EDITOR
+using UnityEngine;
 using UnityEditor;
 
 public class MenuItems
@@ -9,3 +10,4 @@ public class MenuItems
         PlayerPrefs.DeleteAll();
     }
 }
+#endif
