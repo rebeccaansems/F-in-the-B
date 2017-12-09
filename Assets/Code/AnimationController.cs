e6351daa1e02769b5c318873fb4263acb63a635f
Rebecca Ansems <ansems_rebecca@hotmail.com>
2017-12-09 15:59:50 -0400

    Added animations to hint, clear and gem buttons

---
+++ Assets/Code/AnimationController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationController : MonoBehaviour
+{
+    private Animator animator;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+
+        animator.SetBool("AnimateIn", true);
+    }
+}
