2a31021c9292822c52209a61ce5d1ed9058ac502
Rebecca Ansems <ansems_rebecca@hotmail.com>
2018-01-03 18:08:38 -0400

    Added audio to tiles coming in

---
+++ Assets/Code/PlayAudio.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayAudio : MonoBehaviour {
+
+    public int CurrentAudioClip = 0;
+    public AudioClip[] AudioClips;
+    
+    public void Play()
+    {
+        if(AudioClips.Length > 0)
+        {
+            AudioSource.PlayClipAtPoint(AudioClips[CurrentAudioClip], Camera.main.transform.position, 0.75f);
+        }
+    }
+}
