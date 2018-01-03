125198d93a7dc68edc7268c8dd53f0c1d62bca82
Rebecca Ansems <ansems_rebecca@hotmail.com>
2018-01-03 18:44:49 -0400

    Added audio to panels in/out and added code to play audio

--- Assets/Code/PlayAudio.cs
+++ Assets/Code/PlayAudio.cs
@@ -4,14 +4,22 @@ using UnityEngine;
 
 public class PlayAudio : MonoBehaviour {
 
-    public int CurrentAudioClip = 0;
     public AudioClip[] AudioClips;
-    
+
+    private int currentAudioClip = 0;
+
     public void Play()
     {
         if(AudioClips.Length > 0)
-        {
-            AudioSource.PlayClipAtPoint(AudioClips[CurrentAudioClip], Camera.main.transform.position, 0.75f);
+        { 
+            AudioSource.PlayClipAtPoint(AudioClips[currentAudioClip], Camera.main.transform.position, 0.75f);
         }
+
+        currentAudioClip = 0;
     }
+
+    public void ChangeAudio(int num)
+    {
+        currentAudioClip = num;
+    } 
 }
