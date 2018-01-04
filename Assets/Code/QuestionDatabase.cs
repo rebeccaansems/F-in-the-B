3f06a6d7693584f5bc6e483ebf6499ccb166f497
Rebecca Ansems <ansems_rebecca@hotmail.com>
2018-01-04 11:32:45 -0400

    Removed reference to UnityEditor

--- Assets/Code/QuestionDatabase.cs
+++ Assets/Code/QuestionDatabase.cs
@@ -4,7 +4,6 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text.RegularExpressions;
-using UnityEditor;
 using UnityEngine;
 
 public class QuestionDatabase : MonoBehaviour
