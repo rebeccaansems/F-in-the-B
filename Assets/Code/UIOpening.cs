0893ef8b7192d5fccc810905bebe2bdfb4e41dd8
Rebecca Ansems <ansems_rebecca@hotmail.com>
2018-01-04 12:00:41 -0400

    Load scene non-additive

--- Assets/Code/UIOpening.cs
+++ Assets/Code/UIOpening.cs
@@ -4,18 +4,8 @@ using UnityEngine;
 
 public class UIOpening : UI
 {
-
     public void StartGame()
     {
-        SceneManager.LoadScene(1, LoadSceneMode.Additive);
-        SceneManager.sceneLoaded += OnSceneLoaded;
-    }
-
-    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
-    {
-        if (SceneManager.GetSceneByBuildIndex(0).IsValid())
-        {
-            SceneManager.UnloadSceneAsync(0);
-        }
+        SceneManager.LoadScene(1);
     }
 }
