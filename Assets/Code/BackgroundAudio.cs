888d93b88e4fda88f0c91d170174f8bab9e6e49d
Rebecca Ansems <ansems_rebecca@hotmail.com>
2018-01-03 20:53:59 -0400

    Added looping background audio

---
+++ Assets/Code/BackgroundAudio.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundAudio : MonoBehaviour
+{
+    public AudioClip[] BackgroundMusic;
+
+    private AudioSource aud;
+    private float currVolume;
+    private int currSong;
+
+    private static BackgroundAudio bgAudioObject;
+
+    void Awake()
+    {
+        DontDestroyOnLoad(this);
+
+        if (bgAudioObject == null)
+        {
+            bgAudioObject = this;
+        }
+        else
+        {
+            DestroyObject(gameObject);
+        }
+    }
+
+    void Start()
+    {
+        currSong = Random.Range(0, BackgroundMusic.Length);
+        aud = GetComponent<AudioSource>();
+
+        if (!aud.isPlaying)
+        {
+            aud.clip = BackgroundMusic[currSong];
+            aud.playOnAwake = true;
+            aud.loop = true;
+            aud.Play();
+            aud.volume = PlayerStats.s_MusicAudio;
+            currVolume = PlayerStats.s_MusicAudio;
+
+            StartCoroutine(PlayBackgroundMusic());
+        }
+    }
+
+    IEnumerator PlayBackgroundMusic()
+    {
+        yield return new WaitForSeconds(aud.clip.length);
+        Debug.Log("NEW");
+        currSong = Random.Range(0, BackgroundMusic.Length);
+        aud.clip = BackgroundMusic[currSong];
+        aud.Play();
+        PlayBackgroundMusic();
+    }
+
+    public void Update()
+    {
+        if (currVolume != PlayerStats.s_MusicAudio)
+        {
+            aud.volume = PlayerStats.s_MusicAudio;
+            currVolume = PlayerStats.s_MusicAudio;
+        }
+    }
+}
