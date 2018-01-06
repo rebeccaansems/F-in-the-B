cd52699371340ba7d2793affb7e4f7f0063d73f6
Rebecca Ansems <ansems_rebecca@hotmail.com>
2018-01-05 20:49:15 -0400

    Still trying to fix IAP breaking everything

--- Assets/Plugins/UnityPurchasing/script/PurchasingCheck.cs
+++ Assets/Plugins/UnityPurchasing/script/PurchasingCheck.cs
@@ -1,3 +1,5 @@
-﻿#if !UNITY_PURCHASING
+﻿#define UNITY_PURCHASING
+
+#if !UNITY_PURCHASING
 #warning "Unity IAP plugin is installed, but Unity IAP is not enabled. Please enable Unity IAP in the Services window."
 #endif
