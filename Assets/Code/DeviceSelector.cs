1998b777fe4d349911c4d1e4925a0704dffcf999
Rebecca Ansems <ansems_rebecca@hotmail.com>
2018-01-27 14:55:35 -0400

    Changed to checking for phone or tablet via aspect ratio

--- Assets/Code/DeviceSelector.cs
+++ Assets/Code/DeviceSelector.cs
@@ -5,10 +5,10 @@ public class DeviceSelector : MonoBehaviour
     public static int DEVICE;
     public GameObject TabletBackground, TabletGame, PhoneBackground, PhoneGame;
 
-    void Awake()
+    void Awake()    
     {
-#if UNITY_IOS && !UNITY_EDITOR
-        if (UnityEngine.iOS.Device.generation.ToString().Contains("iPad"))
+        Debug.Log(Camera.main.aspect);
+        if (Camera.main.aspect > 0.6f)
         {
             DEVICE = 1;
             Destroy(PhoneBackground);
@@ -22,11 +22,5 @@ public class DeviceSelector : MonoBehaviour
             Destroy(TabletGame);
             PhoneBackground.GetComponent<BackgroundSelection>().DontDestroy();
         }
-#else
-        DEVICE = 0;
-        Destroy(TabletBackground);
-        Destroy(TabletGame);
-        PhoneBackground.GetComponent<BackgroundSelection>().DontDestroy();
-#endif
     }
 }
